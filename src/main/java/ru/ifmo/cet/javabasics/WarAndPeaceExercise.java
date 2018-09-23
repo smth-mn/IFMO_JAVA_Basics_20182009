6c6c94c6b4f143fdf883cb4b208e046f8fe18248
Svyatoslav Yakovlev <svyatoslav146@gmail.com>
2018-09-24 02:20:22 +0300

    Parser of WAP with streams

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,12 +1,22 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    private static final String parsingRegexp =
+            "\\s|\\,|\\.|\\;|\\:|\\!|\\(|\\)|/|\"|\'|\\?|>|<" +
+            "|«|»|\\…|\\]|\\[|\\„|1|2|3|4|5|6|7|8|9|0|-|“";
+
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
@@ -15,7 +25,23 @@ public class WarAndPeaceExercise {
         // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
         // TODO Also omit any word with lengths less than 4 and frequency less than 10
 
-        throw new UnsupportedOperationException();
+        return Stream.concat(
+                Files.readAllLines(tome12Path, Charset.forName("Cp1251")).stream(),
+                Files.readAllLines(tome34Path, Charset.forName("Cp1251")).stream())
+                .flatMap((line) -> Arrays.stream(line.split(parsingRegexp)))
+                .map(String::toLowerCase)
+                .filter( word -> word.length() > 3)
+                .collect(Collectors.toMap( word -> word,
+                        word -> 1,
+                        (number, anotherNumber) -> number + anotherNumber))
+                .entrySet().stream().filter((entry) -> entry.getValue() >=10)
+                .sorted( (entry, anotherEntry) ->
+                    entry.getValue() - anotherEntry.getValue() == 0 ?
+                        entry.getKey().compareTo(anotherEntry.getKey()) :
+                        anotherEntry.getValue() - entry.getValue()
+                )
+                .map( entry -> entry.getKey() + " - " + entry.getValue() + "\n")
+                .reduce( (string, anotherString) -> string + anotherString).get().trim();
     }
 
 }
